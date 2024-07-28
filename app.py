928b007c23c3ee6752ffe126898bcb451481a422
QuantumCoder <gianlucaproto@gmail.com>
2024-07-28 02:29:52 +0200

    feat(1) - aggiunto font lobster, sistemazioni grafiche e sicurezza immagini

--- app.py
+++ app.py
@@ -7,12 +7,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/art-gallery')
-def gallery():
-    image_folder = os.path.join(app.static_folder, 'img/gallery')
-    images = [f for f in os.listdir(image_folder) if
-              os.path.isfile(os.path.join(image_folder, f))]
-    return render_template('gallery.html', images=images)
+
 
 @app.route('/commissions')
 def commissions():
@@ -25,6 +20,30 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+def get_images_from_folder(folder, category):
+    image_list = []
+    for filename in os.listdir(folder):
+        if filename.endswith(('.png', '.jpg', '.jpeg', '.gif')):
+            image_path = os.path.join('img/gallery', category, filename)
+            image_list.append({'filename': image_path, 'category': category})
+    return image_list
+
+@app.route('/art-gallery')
+def art_gallery():
+    gallery_folder = os.path.join(app.static_folder, 'img/gallery')
+
+    animals_folder = os.path.join(gallery_folder, 'animals')
+    comics_folder = os.path.join(gallery_folder, 'comics')
+    illustrations_folder = os.path.join(gallery_folder, 'illustrations')
+
+    images = get_images_from_folder(animals_folder, 'animals')
+    images += get_images_from_folder(comics_folder, 'comics')
+    images += get_images_from_folder(illustrations_folder, 'illustrations')
+
+    return render_template('gallery.html', images=images)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
